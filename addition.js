ee09949167b613a382abdf28b4df8af3fdac1c47
Passang-Dorji <pssngdor94@gmail.com>
2024-02-13 01:22:35 -0800

    simple array coding

--- addition.js
+++ addition.js
@@ -1,6 +1,5 @@
 var rl = require("readline-sync")
 var a = rl.question("give me a number?")
 var b = rl.question("give me another number?")
- 
-console.log(Number(a) +Number(b))
+ console.log(Number(a) +Number(b))
 
---
+++ largestarray.js
@@ -0,0 +1,20 @@
+ //const numbers = [12, 44, 9.8473, 30, -12, 57382, 1, 0.3422]
+// var largestnum=numbers[0]
+// for(var i=0;i<numbers.length;i++){
+//     if(numbers[i]>largestnum) largestnum=numbers[i]
+// }
+// console.log(largestnum)
+
+// console.log(Math.max(...numbers))//shortcut
+ //console.log(Math.min(...numbers))
+//  numbers.sort(function(a, b) {
+//             return b - a;
+//         });
+
+//         console.log(numbers);
+const alphabet=['a','c','d','f','g']
+// var a=alphabet.sort()
+// console.log(a)
+a=alphabet.sort()
+a.reverse()
+console.log(a)
